61d3441fb97d90f5e4bf46c1d6654c2836a7b1db
Amy Groshek <amygroshek@gmail.com>
2018-05-25 16:53:26 -0500

    Add checkboxes and readme

---
+++ htmltotext.js
@@ -0,0 +1,39 @@
+(function ($) {
+    $('#convert').click(function() {
+        // console.log('click');
+        // If there's html to convert, call function and provide contents.
+        var html = $('#html_input').val();
+        if (html.length >= 1) {
+            // console.log('There is content to convert, proceeding... ');
+            // html.replace("<", "&lt;");
+            html = html.replace(/&/g, "&amp;")
+                .replace(/</g, "&lt;")
+                .replace(/>/g, "&gt;");
+            // console.log(html);
+            if ($('#wrap_in_code').is(":checked")) {
+                // console.log('wrap in code checked');
+                html = '<code>' + html + '</code>';
+            }
+            // console.log(html);
+            $('#html_input').val(html);
+            if ($('#copy_to_clipboard').is(":checked")) {
+                // console.log('autocopy checked');
+                $('#html_input').select();
+                document.execCommand('copy');
+                $('#html_input').val('');
+            } else {
+                // console.log('autocopy NOT checked');
+                $('#copy')
+                    .prop('disabled', false)
+                    .click(function(e) {
+                        // Copy the result to the clipboard on click.
+                        $('#html_input').select();
+                        document.execCommand('copy');
+                        $('#html_input').val('');
+                        // Remove event listener
+                        $(e.currentTarget).unbind('click');
+                    });
+            }
+        }
+    });
+})(jQuery);
